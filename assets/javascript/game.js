76047b009d0ce2aa781c4763d3c8ec263f0e51d4
Xingxian Shou <Xingxian Shou>
2017-05-06 20:35:43 -0400

    fixed the display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,20 +2,38 @@
         'bucks', 'pacers','bulls','pistons','wizard','hawks','heat',
         'hornets','mavericks','jazz','thunders','nuggets','warriors','clippers',
         'grizzles','timberwovles','pelicans','knicks','spurs','lakers'];
-    var word;
-    var correctWord = [];
-    var guess;
-    var guessStored = [];
-    var lives;
-    var wins;
-    var space; 
+    
     console.log(teamNames.length);
+      var wins=0;
+      var word;
+      var correctWord = [];
+      var guess;
+      var guessStored=[];
+      var guessStoredWord;
+      var usedLetter = [];
+      var isOver=0;
+      var space;
+      var lives = 10;
+
+
+    reset = function(){
+      var word;
+      var correctWord = [];
+      var guess;
+      var guessStored=[];
+      var guessStoredWord;
+      var usedLetter = [];
+      var isOver=0;
+      var space;
+      var lives = 10;
+    }
+
+    reset();
 
     play = function(){
       word = teamNames[Math.floor(Math.random() * teamNames.length)];
-      word = word.replace(/\s/g, "-");
       console.log(word);
-      for(var i=0; i<word.length; i++){
+       for(var i=0; i<word.length; i++){
           
            correctWord[i] = word[i];
           
@@ -23,30 +41,41 @@
       console.log(correctWord);
     }
 
-    lives = 5;
     var showlives = document.getElementById("livesLeft");
+    var usedHolder = document.getElementById("guessedLetter");
+    var winsTotal = document.getElementById("wins");
     // show lives
-    comments = function(){
-      showlives.innerHTML = "You have " + lives + " lives left!";
-      if(lives < 1)
+      comments = function(){
+      showlives.innerHTML = "# of guessing remaining: " + lives;
+      usedHolder.innerHTML = usedLetter;
+      if(lives < 1){
         showlives.innerHTML = "Game over!";
-      if(lives > 0 && 1)
-        showlives.innerHTML = "";  
+        document.onkeyup = "";
+      }
+
+      if(lives > 0 && isOver){
+        showlives.innerHTML = "# of guessing remaining: " + lives + "! ~~You win!~~"; 
+        wins += 1;
+        document.onkeyup = "";
+
+      }
+
+      winsTotal.innerHTML = wins;
     }
 
-   // Create geusses ul
+   
    result = function () {
-    wordHolder = document.getElementById('hold');
-    correct = document.createElement('ul');
-
-    for (var i = 0; i < word.length; i++) {
+     var  wordHolder = document.getElementById('hold');
+    
+        for (var i = 0; i < word.length; i++) {
         guessStored.push('_ ');
     }
-    wordHolder.innerHTML = guessStored;
+    guessStoredWord = guessStored.join("");
+    wordHolder.innerHTML = guessStoredWord;
   }
 
- 
-  console.log(guessStored);
+    play();
+    result();
 
    document.onkeyup = function(event) {
 
@@ -54,32 +83,46 @@
         var userGuess = event.key;
         var guessWord;
                // Alerts the key the user pressed (userGuess).
-        alert(userGuess);
+        // alert(userGuess);
+
+        if(correctWord.indexOf(userGuess) === -1){
+            lives -= 1;
+        }
+        console.log("lives:" + lives)
+        console.log(correctWord);
 
         for(var i=0; i<word.length; i++){  //"_ _ _ "
           if(userGuess === word[i]){
                 guessStored[i] = word[i];
           }
         }
-        wordHolder.innerHTML = guessStored;
+        guessStoredWord = guessStored.join("");
+        wordHolder.innerHTML = guessStoredWord;
         console.log("guess word is " + guessStored);
 
-        for(var i=0; i<word.length; i++){  //"_ _ _ "
-                guessWord += guessStored[i];
-        }
+        guessWord = guessStored.join("");
         console.log(guessWord);
         if(guessWord === word)
         {
-          alert("You Win!");
-          document.onkeyup = " ";
+          isOver = true;
         }
+        else
+        {
+
+        }
+        //store the used letter and display them
+        if(usedLetter.indexOf(userGuess) === -1)
+        {
+          usedLetter.push(userGuess);
+        }
+        comments();
+
+        if(0){
+        reset();
+        console.log("reset the isOver value to be: " + isOver)
+        play();
+        result();
+        console.log("after win, new word:" + word);
+        console.log("after win, reset:" + guessStoredWord);
       }
-
-    play();
-    result();
-    comments();
-    //document.onkeyup();
-
-    // type function
-
-   
+  }
